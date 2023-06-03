3319ef81185455e8b1964a815736627b7a69fd1d
Guille Gelso <celidiog@gmail.com>
2023-06-03 12:51:01 -0300

    para entregar

--- script.js
+++ script.js
@@ -10,48 +10,85 @@ let prec3=320
 let prec4=420
 let prec5=29
 
+let itienda= ""
+let compra= ""
+let compractad= ""
+let subtotal= ""
+let agregar=""
+let producto=""
+
+
+itienda = Number(prompt("Ingrese 1 para ingresar a tienda" + "\n" + "Ingrese 2 para salir"))
 
 
 
-
-let itienda = Number(prompt("Ingrese 1 para ingresar a tienda" + "\n" + "Ingrese 2 para salir"))
-
-if (itienda===1) {
- let compra= Number(prompt("Seleccione el producto " + "\n" + "1-" + prod1 + " " + "$" + prec1 + "\n" + "2-" + prod2 + " " + "$" + prec2 + "\n" + "3-" + prod3 + " "+ "$" + prec3 + "\n" + "4-" + prod4 + " " + "$"+ prec4 + "\n" + "5-" + prod5 + " "+ "$"+ prec5 ))
- 
- 
- if (compra===1){
-  let compractad= Number(prompt("Ingrese cantidad"))
-  let subtotal= compractad * prec1
-  alert (prod1 + " "+ "$"+ subtotal)
-}
-  if (compra===2){
-    let compractad= Number(prompt("Ingrese cantidad"))
-    let subtotal= compractad * prec2
-    alert (prod2 + " "+ "$"+ subtotal)   
- }
-
- if (compra===3){
-  let compractad= Number(prompt("Ingrese cantidad"))
-  let subtotal= compractad * prec3
-  alert (prod3 + " "+ "$"+ subtotal)   
-}
-
-if (compra===4){
-  let compractad= Number(prompt("Ingrese cantidad"))
-  let subtotal= compractad * prec4
-  alert (prod4 + " "+ "$"+ subtotal)   
-}
- 
-if (compra===5){
-  let compractad= Number(prompt("Ingrese cantidad"))
-  let subtotal= compractad * prec5
-  alert (prod5 + " "+ "$"+ subtotal)   
-}
-}
+  if (itienda===2 ) {
+    alert ("Gracias por visitarnos")
+    }
 else {
-  alert("Gracias por visitar tienda")
+  
+do {
+  
+  
+    compra= Number(prompt("Seleccione el producto " + "\n" + "1-" + prod1 + " " + "$" + prec1 + "\n" + "2-" + prod2 + " " + "$" + prec2 + "\n" + "3-" + prod3 + " "+ "$" + prec3 + "\n" + "4-" + prod4 + " " + "$"+ prec4 + "\n" + "5-" + prod5 + " "+ "$"+ prec5 ))
+    compractad= Number(prompt("Ingrese cantidad"))
+  
+    
+   
+    if (compra===1){
+    
+      subtotal= compractad * prec1
+      producto=prod1
+      salida()  
+    }
+     
+      else if  (compra===2){
+        
+        subtotal= compractad * prec2
+        producto=prod2
+        salida()    
+     }
+    
+     else if (compra===3){
+      
+      subtotal= compractad * prec3
+      producto=prod3
+      salida()    
+    }
+    
+      else if  (compra===4){
+    
+      subtotal= compractad * prec4
+      producto=prod4
+      salida()    
+    }
+     
+    else if (compra===5){
+    
+      subtotal= compractad * prec5
+      producto=prod5
+      salida()  
+    }
+
+    function salida() {
+      let precio=subtotal
+      alert(producto + " "+ "$"+ precio)
+      
+    }
+
+    agregar=Number(prompt("Ingrese 1 para Agregar" + "\n" + "Ingrese 2 para salir"))
+ 
+  
+
+} while (agregar===1 ){
+  
+  if (agregar===2){
+    alert("Gracias por su compra")
+   }
 }
+}
+
+console.log("salida")
 
 
 
